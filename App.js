292662ef472476882b9c68b77691231a91e7ec35
Jeremy <jsoon049@uottawa.ca>
2021-02-06 13:39:53 -0500

    Added location

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  //const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+      (async () => {
+        let locationEnabled = await Location.hasServicesEnabledAsync();
+        if (!locationEnabled){
+          alert("Turn on your location services to use this app");
+        }
+        else{
+          let { status } = await Location.requestPermissionsAsync();
+          if (status === 'granted') {
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+          }
+          else{
+            //setErrorMsg('Permission to access location was denied');
+            alert('Allow access to location services to use this app');
+          }
+        }
+      })();
+  }, []);
+
+  let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else 
+  if (location) {
+    console.log(location);
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
   );
 }
@@ -14,8 +46,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
